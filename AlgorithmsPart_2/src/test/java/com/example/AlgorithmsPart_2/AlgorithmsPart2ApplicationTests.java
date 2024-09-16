ff7049357e534ee98c71b89d09cf9bbaf5bd674c
Pavel <pavelpulle1@gmail.co>
2024-09-16 23:08:53 +0300

    Initial commit

---
+++ AlgorithmsPart_2/src/test/java/com/example/AlgorithmsPart_2/AlgorithmsPart2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AlgorithmsPart_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgorithmsPart2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
